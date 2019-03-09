e2a0fa69d9b7b77da8e8e0689e38e26a8f2173b3
Phillip Boateng <cnolimit@gmail.com>
2019-03-09 17:01:18 +0000

    cleaning up the boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Dashboard from './layouts/Dashboard'
+import { createGlobalStyle } from 'styled-components'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
   }
-}
+`
 
-export default App;
+const App = () => (
+  <React.Fragment>
+    <GlobalStyle />
+    <Dashboard />
+  </React.Fragment>
+)
+
+export default App
